6bb1402c103eec7c93f4233c694feea9bfe96cb6
AlessandroVe <aveneziano536@gmail.com>
2021-06-16 06:55:14 +0200

    .

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare
 PARTE 3
 Dare la possibilità all’utente, attraverso 3 prompt() - uno per ciascuna proprietà - di aggiungere UN SOLO oggetto studente inserendo nell’ordine: nome, cognome e età. */
 document.addEventListener("DOMContentLoaded", function() {
-
+    var output = document.getElementById("output");
     // PARTE 1
     //Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell’oggetto.
     
@@ -17,7 +17,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     }
     for(var key in studente){    
-        console.log(studente[key]);
+       output.innerHTML += "<li>"+ studente[key]+ "</li>";
     }
-
+    // PARTE 2
+    // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.  Ricordatevi, è un array, quindi? for.
 })
